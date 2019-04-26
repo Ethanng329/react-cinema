58ee93b0ad32deeb4ab3c118a63682f849fd08b1
Ethan Ng <ethanng329@gmail.com>
2019-04-26 10:35:46 +0100

    restyle plot display

--- src/Plot.js
+++ src/Plot.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import styled from 'styled-components';
+import { withStyles } from '@material-ui/core/styles';
 
+const PaperContainer = styled.div`
+  display: flex;
+  height: 9vh;
+  justify-items: center;
+`;
+
+const style = {
+  text: {
+    textAlign: 'center',
+    flex: 1,
+    alignItems: 'center',
+    alignSelf: 'center',
+    backgroundColor: '#dbdbdb'
+  }
+};
 class Plot extends React.Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div className="plotfooter">
-        <p>Plot:{this.props.plot}</p>
-      </div>
-
-    )
-
+      <PaperContainer>
+        <Paper className={classes.text}>Plot:{this.props.plot}</Paper>
+      </PaperContainer>
+      // <div className="plotfooter">
+      //   <p>Plot:{this.props.plot}</p>
+      // </div>
+    );
   }
 }
 
-
-export default Plot;
+export default withStyles(style)(Plot);
